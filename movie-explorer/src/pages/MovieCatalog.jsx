b51f80c300e3d320f57ce6bf82e1e9763994a9ac
SimonaB0895 <b.simona.moni1209@gmail.com>
2025-05-05 19:35:13 +0300

    vite and plugins installed so API can fUnctionate like it's Supposed To :)

--- movie-explorer/src/pages/MovieCatalog.jsx
+++ movie-explorer/src/pages/MovieCatalog.jsx
@@ -4,17 +4,13 @@ import { Link } from "react-router-dom";
 function MovieCatalog() {
   const [movies, setMovies] = useState([]);
   //const [search, setSearch] = useState("");
-  const [searchTerm, setSearchTerm] = useState("batman"); // По подразбиране
+  const [searchTerm, setSearchTerm] = useState("Star Wars"); // По подразбиране
 
 
   const apiKey = import.meta.env.VITE_OMDB_API_KEY;
 
   useEffect(() => {
-   // fetch("http://www.omdbapi.com/?s=batman&apikey=REACT_APP_OMDB_API_KEY")
    fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=${apiKey}`)
-  //     .then(res => res.json())
-  //     .then(data => setMovies(data.Search || []));
-  // }, []);
   .then((res) => res.json())
       .then((data) => {
         if (data.Search) {
@@ -26,14 +22,13 @@ function MovieCatalog() {
   const filtered = movies.filter(m =>
     m.Title.toLowerCase().includes(searchTerm.toLowerCase())
   );
-  
+
 
   return (
     <div>
       <h2>Каталог с филми</h2>
       <input
         type="text"
-        placeholder="Търси филм..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
