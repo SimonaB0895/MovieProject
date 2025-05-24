e39c4b1f64840cdbfda0acff8113169cc25b4dd4
SimonaB0895 <b.simona.moni1209@gmail.com>
2025-05-25 01:10:27 +0300

    Profile component modified and CSS added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import "./Profile.css"; 
+
 
 function Profile() {
   const { user } = useContext(AuthContext);
@@ -7,9 +9,10 @@ function Profile() {
   if (!user) return <p>Няма активен потребител</p>;
 
   return (
-    <div>
-      <h2>Профил</h2>
-      <p>Имейл: {user.email}</p>
+     <div className="profile-container">
+      <h2>Profile</h2>
+      <p>Username: {user.username}</p>
+      <p>E-mail: {user.email}</p>
     </div>
   );
 }
